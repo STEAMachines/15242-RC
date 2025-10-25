e324f6a62f5729cc97b66e0f18bce8fc4bca8ba9
STEAMachines <steamachines_42@outlook.com>
2025-10-25 19:39:01 +0700

    UPDATE, 25/10/2025, 19:38PM

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# 15242-RC
-STEAMachines-RobotController
+# STEAMachines-RC
+
+Welcome to **STEAMachines-RC** Pages, this is the official code from robotics team called
+***STEAMachines #15242*** from Yogyakarta, Indonesia that compete in FTC IFR Nusantara Regional.
+
+---
+
+## How to Download This Project
+
+You can get this repository in two ways:
+### Option 1 — Download as ZIP
+1. Go to this repository page on GitHub:  
+   👉 [https://github.com/YourUsername/STEAMachines-RC](https://github.com/YourUsername/STEAMachines-RC)
+2. Click the green **“Code”** button near the top right.
+3. Select **“Download ZIP”**.
+4. After downloading, extract the ZIP file to your computer.
+5. Open the folder in **Android Studio** via  
+   `File → Open → Select Folder`.
+
+---
+
+### Option 2 — Clone using Git
+If you have Git installed, open your terminal or Git Bash and run:
+```bash
+git clone https://github.com/YourUsername/STEAMachines-RC.git
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDrive3_MeepMeep.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.part_robot.RollerIntake_EncoderBased;
+import org.firstinspires.ftc.teamcode.part_robot.Shooter_EncoderBased;
+
+@Autonomous(name="AutoDrive3_MeepMeep_EncoderBased", group="STEAMachines_DECODE")
+public class AutoDrive3_MeepMeep extends LinearOpMode {
+    public void runOpMode() {
+        Pose2d beginPose = new Pose2d(-48, 48, -Math.PI/4);
+        TankDrive drive = new TankDrive(hardwareMap, beginPose);
+        RollerIntake_EncoderBased intake = new RollerIntake_EncoderBased(hardwareMap);
+        Shooter_EncoderBased shooter = new Shooter_EncoderBased(hardwareMap);
+        waitForStart();
+        if(!isStopRequested()) return;
+        Actions.runBlocking(
+                drive.actionBuilder(beginPose)
+                        .build());
+    }
+}
